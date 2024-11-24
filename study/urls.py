60ceb74f0e3cb12be2a7fef4ec4a452f43f04a26
Douglas Lima <douglas.Lima@AB-inbev.com>
2024-11-24 16:28:26 -0300

    Renaming of app generator to study, to serve as reference

--- study/urls.py
+++ study/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from generator.views.github import GitHubProjectsView
-from generator.views.helloWorld import hello_world
+from study.views.github import GitHubProjectsView
+from study.views.helloWorld import hello_world
 
 urlpatterns = [
     path('', hello_world, name='hello_world'),
--- study/views/github.py
+++ study/views/github.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import requests
-from generator.serializers.github import GitHubRepoSerializer
+from study.serializers.github import GitHubRepoSerializer
 
 class GitHubProjectsView(APIView):
     def get(self, request, username):
