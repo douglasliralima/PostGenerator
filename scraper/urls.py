b517f2a3ce0ba21f81442ddfd3a6541ee3080ce7
Douglas Lima <douglas.Lima@AB-inbev.com>
2024-11-27 00:07:29 -0300

    Removing twitter thanks to not free official resources

--- scraper/urls.py
+++ scraper/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from scraper.views.reddit import RedditScraperView
-from scraper.views.twitter import TwitterScraperView
 
 urlpatterns = [
     path('reddit/', RedditScraperView.as_view(), name='reddit_scraper'),
-    path('twitter/', TwitterScraperView.as_view(), name='twitter_scraper'),
 ]
--- scraper/views/twitter.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from scraper.services.getTwitterPosts import get_popular_tweets
-
-class TwitterScraperView(APIView):
-    def get(self, request):
-        query = request.GET.get('query', 'meme')
-        tweets = get_popular_tweets(query=query)
-        return Response({"tweets": tweets})
